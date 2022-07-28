01350adae93431a541de2dd3b8abacdd7606da0a
Diogo Castro <diogocastro.dev@gmail.com>
2022-07-28 10:52:34 +0100

    Meta Tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@styles/style.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { PastContextProvider } from "../context/PastContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <PastContextProvider>
+        <Head>
+          <title>Diogo Castro's Portfolio</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="title" content="Diogo Castro's Portfolio" />
+          <meta name="description" content="Know more about me" />
+          <meta name="keywords" content="portfolio" />
+        </Head>
         <Component {...pageProps} />
       </PastContextProvider>
     </>
